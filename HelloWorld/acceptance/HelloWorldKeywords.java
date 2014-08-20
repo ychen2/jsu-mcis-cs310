5719f0bc8faf08bd3df3060ebc63d6a91a165f32
Aaron Garrett <aaron.lee.garrett@gmail.com>
2014-08-20 02:30:38 -0500

    first commit

---
+++ HelloWorld/acceptance/HelloWorldKeywords.java
@@ -0,0 +1,14 @@
+import edu.jsu.mcis.*;
+
+public class HelloWorldKeywords {
+    private HelloWorld helloWorld = new HelloWorld();
+    
+    public String greetEveryone() {
+        return helloWorld.showIt();
+    }
+    
+    public String reverseGreetEveryone() {
+        return helloWorld.reverseIt();
+    }
+    
+}
